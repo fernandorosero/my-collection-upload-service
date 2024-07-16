2367e6e603f5d471367123271cc9374d86aacf3f
macpro <frosero@frosero.com>
2024-07-16 23:44:30 +0200

    first upload

---
+++ src/main/java/com/nrfr/mycollection/upload/service/MyCollectionUploadApplication.java
@@ -0,0 +1,13 @@
+package com.nrfr.mycollection.upload.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCollectionUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCollectionUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nrfr/mycollection/upload/service/MyCollectionUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrfr.mycollection.upload.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCollectionUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
