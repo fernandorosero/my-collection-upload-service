012f49f1a2ab29310d7818ba0a41a26ef7e14f99
fernandorosero <tatofarp@hotmail.com>
2024-07-17 15:39:51 +0000

    Mensaje de saludo

--- src/main/java/com/nrfr/mycollection/upload/service/adapter/in/rest/UploadController.java
+++ src/main/java/com/nrfr/mycollection/upload/service/adapter/in/rest/UploadController.java
@@ -3,11 +3,15 @@ package com.nrfr.mycollection.upload.service.adapter.in.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class UploadController {
 
     @GetMapping(path ="/api/v1/upload")
     public String getSaludo() {
+        log.info("Llamada al servicio");  
         return "Holita desde mi ipad pro :)";
     }
 
